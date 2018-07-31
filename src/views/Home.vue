cdf9473698adce6bf03061de81085214e4dc3b34
android <android>
2018-07-31 15:55:15 +0800

    优化UI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,9 +16,10 @@
           <!-- <span class="el-dropdown-link userinfo-inner"><img src="../assets/user.png"/> {{sysUserName}}</span> -->
           <span class="el-dropdown-link userinfo-inner"><img src="../assets/user.png"/> </span>
           <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>我的消息</el-dropdown-item>
-            <el-dropdown-item>设置</el-dropdown-item>
-            <el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>
+            <!--<el-dropdown-item>我的消息</el-dropdown-item>-->
+            <!--<el-dropdown-item>设置</el-dropdown-item>-->
+            <!--<el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>-->
+            <el-dropdown-item @click.native="logout">退出登录</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
       </el-col>
