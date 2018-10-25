3106aa2bfe04bd00f6612ba791f635ee65d4c06e
android <android>
2018-10-25 18:05:32 +0800

    优化首页滚动条显示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,53 @@
 <template>
   <el-container style="height: 100%;">
-      <el-menu class="el-menu-nav" index="0" :collapse="isCollapse" :router="true" default-active="1">
-        <!--<el-menu-item disabled><i class="fa fa-flash" :style="isCollapse ? 'font-size:22px' : 'font-size:14px'"/><span slot="title" >雷鸣外呼管理平台</span></el-menu-item>-->
-        <el-menu-item index="0" disabled style="cursor:default"><i class="fa fa-flash"/><span slot="title" >雷鸣外呼管理平台</span></el-menu-item>
-        <el-menu-item index="1" route="/dashboard"><i class="fa fa-home"/><span slot="title">首页-工作台</span></el-menu-item>
-        <el-menu-item index="2" v-if="isSuperAdmin === 'false'" route="/account"><i class="iconfont icon-zhanghu"/><span slot="title">账户管理</span></el-menu-item>
-        <el-menu-item index="3" route="/company"><i class="iconfont icon-gongsiguanli"/><span slot="title">公司管理</span></el-menu-item>
-        <el-menu-item index="4" route="/salesman"><i class="iconfont icon-xiaoshoushu"/><span slot="title">销售管理</span></el-menu-item>
-        <el-menu-item index="5" route="/list"><i class="iconfont icon-guize"/><span slot="title">名单管理</span></el-menu-item>
-        <el-menu-item index="6" route="/task"><i class="fa fa-phone"/><span slot="title">任务管理</span></el-menu-item>
-        <el-submenu index="7">
-          <template slot="title"><i class="iconfont icon-xiaoxishezhi" style="color: #ffffff"></i><span slot="title" style="color: #ffffff">推送管理</span></template>
-          <el-menu-item index="7-1" route="/push/rule"><i class="iconfont icon-guize"/><span slot="title">推送规则</span></el-menu-item>
-          <el-menu-item index="7-2" route="/push/push"><i class="iconfont icon-fasong"/><span slot="title">信息推送</span></el-menu-item>
-        </el-submenu>
-        <el-menu-item index="8" v-if="isSuperAdmin === 'true'" route="/trial"><i class="fa fa-list-ul"/><span slot="title">试用管理</span></el-menu-item>
-        <el-menu-item index="9" v-if="isSuperAdmin === 'true'" route="/check"><i class="iconfont icon-shenheguanli"/><span slot="title">审核管理</span></el-menu-item>
-        <el-submenu index="10">
-          <template slot="title"><i class="iconfont icon-chart" style="color: #ffffff"></i><span slot="title" style="color: #ffffff">报表统计</span></template>
-          <el-menu-item index="10-1" route="/report/workload"><i class="iconfont icon-wodegongzuoliang"/><span slot="title">工作量统计</span></el-menu-item>
-          <el-menu-item index="10-2" route="/report/task"><i class="iconfont icon-boda"/><span slot="title">外呼任务统计</span></el-menu-item>
-          <el-menu-item index="10-3" route="/report/count"><i class="iconfont icon-lishi"/><span slot="title">话务统计</span></el-menu-item>
-          <el-menu-item index="10-4" route="/report/record"><i class="iconfont icon-tonghuahuawuliang"/><span slot="title">历史通话记录</span></el-menu-item>
-        </el-submenu>
-        <el-menu-item index="11" v-if="isSuperAdmin === 'true'" route="/system"><i class="iconfont icon-quanxian"/><span slot="title">系统管理</span></el-menu-item>
-        <el-menu-item index="12" v-if="isSuperAdmin === 'true'" route="/feedback"><i class="fa fa-question-circle"/><span slot="title">问题反馈</span></el-menu-item>
-        <el-menu-item index="13" v-if="isSuperAdmin === 'true'" route="/version/list"><i class="iconfont icon-banben"/><span slot="title">版本管理</span></el-menu-item>
-      </el-menu>
-    <el-container style="width: 90%">
+    <el-menu class="el-menu-nav" index="0" :collapse="isCollapse" :router="true" default-active="1" @open="handleChange"
+             @close="handleChange">
+      <el-menu-item index="0" disabled style="cursor:default"><i class="fa fa-flash"/><span slot="title">雷鸣外呼管理平台</span>
+      </el-menu-item>
+      <el-menu-item index="1" route="/dashboard"><i class="fa fa-home"/><span slot="title">首页-工作台</span></el-menu-item>
+      <el-menu-item index="2" v-if="isSuperAdmin === 'false'" route="/account"><i class="iconfont icon-zhanghu"/><span
+        slot="title">账户管理</span></el-menu-item>
+      <el-menu-item index="3" route="/company"><i class="iconfont icon-gongsiguanli"/><span slot="title">公司管理</span>
+      </el-menu-item>
+      <el-menu-item index="4" route="/salesman"><i class="iconfont icon-xiaoshoushu"/><span slot="title">销售管理</span>
+      </el-menu-item>
+      <el-menu-item index="5" route="/list"><i class="iconfont icon-guize"/><span slot="title">名单管理</span>
+      </el-menu-item>
+      <el-menu-item index="6" route="/task"><i class="fa fa-phone"/><span slot="title">任务管理</span></el-menu-item>
+      <el-submenu index="7">
+        <template slot="title"><i class="iconfont icon-xiaoxishezhi" style="color: #ffffff"></i><span slot="title"
+                                                                                                      style="color: #ffffff">推送管理</span>
+        </template>
+        <el-menu-item index="7-1" route="/push/rule"><i class="iconfont icon-guize"/><span slot="title">推送规则</span>
+        </el-menu-item>
+        <el-menu-item index="7-2" route="/push/push"><i class="iconfont icon-fasong"/><span slot="title">信息推送</span>
+        </el-menu-item>
+      </el-submenu>
+      <el-menu-item index="8" v-if="isSuperAdmin === 'true'" route="/trial"><i class="fa fa-list-ul"/><span
+        slot="title">试用管理</span></el-menu-item>
+      <el-menu-item index="9" v-if="isSuperAdmin === 'true'" route="/check"><i class="iconfont icon-shenheguanli"/><span
+        slot="title">审核管理</span></el-menu-item>
+      <el-submenu index="10">
+        <template slot="title"><i class="iconfont icon-chart" style="color: #ffffff"></i><span slot="title"
+                                                                                               style="color: #ffffff">报表统计</span>
+        </template>
+        <el-menu-item index="10-1" route="/report/workload"><i class="iconfont icon-wodegongzuoliang"/><span
+          slot="title">工作量统计</span></el-menu-item>
+        <el-menu-item index="10-2" route="/report/task"><i class="iconfont icon-boda"/><span slot="title">外呼任务统计</span>
+        </el-menu-item>
+        <el-menu-item index="10-3" route="/report/count"><i class="iconfont icon-lishi"/><span slot="title">话务统计</span>
+        </el-menu-item>
+        <el-menu-item index="10-4" route="/report/record"><i class="iconfont icon-tonghuahuawuliang"/><span
+          slot="title">历史通话记录</span></el-menu-item>
+      </el-submenu>
+      <el-menu-item index="11" v-if="isSuperAdmin === 'true'" route="/system"><i class="iconfont icon-quanxian"/><span
+        slot="title">系统管理</span></el-menu-item>
+      <el-menu-item index="12" v-if="isSuperAdmin === 'true'" route="/feedback"><i class="fa fa-question-circle"/><span
+        slot="title">问题反馈</span></el-menu-item>
+      <el-menu-item index="13" v-if="isSuperAdmin === 'true'" route="/version/list"><i
+        class="iconfont icon-banben"/><span slot="title">版本管理</span></el-menu-item>
+    </el-menu>
+    <el-container :style="{width:'90%',height:pageHeight+'px'}">
       <el-header>
         <div class="hello" @click="collapse" style="cursor:pointer">
           <i :class="isCollapse ? 'iconfont icon-zhankaicaidan' : 'iconfont icon-shouqicaidan'"/>{{now}}好，{{username}}！
@@ -49,36 +69,50 @@
 </template>
 <script>
   export default {
-    data () {
+    data() {
       return {
         now: '凌晨',
         isCollapse: false,
-        isSuperAdmin:'false',
-        username:''
+        isSuperAdmin: 'false',
+        username: '',
+        pageHeight: 0
       }
     },
-    created () {
+    created() {
       let hour = new Date().getHours();
-      if (hour < 5){
+      if (hour < 5) {
         this.now = '凌晨'
-      } else if (hour < 9){
+      } else if (hour < 9) {
         this.now = '早上'
-      } else if (hour < 12){
+      } else if (hour < 12) {
         this.now = '上午'
-      } else if (hour < 14){
+      } else if (hour < 14) {
         this.now = '中午'
-      } else if (hour < 18){
+      } else if (hour < 18) {
         this.now = '下午'
-      } else if (hour < 19){
+      } else if (hour < 19) {
         this.now = '傍晚'
-      } else if (hour < 24){
+      } else if (hour < 24) {
         this.now = '晚上'
       }
       this.username = sessionStorage.getItem('username')
-      this.isSuperAdmin  = sessionStorage.getItem('isSuperAdmin')
+      this.isSuperAdmin = sessionStorage.getItem('isSuperAdmin')
       this.$router.replace('/dashboard')
+      this.handleChange()
     },
     methods: {
+      handleChange() {
+        let that = this
+        let windowHeight = document.documentElement.clientHeight
+        that.pageHeight = windowHeight
+        setTimeout(function () {
+          let pageHeight = document.documentElement.scrollHeight
+          if (pageHeight < windowHeight) {
+            pageHeight = windowHeight
+          }
+          that.pageHeight = pageHeight
+        }, 350)
+      },
       // 退出登录
       logout() {
         this.$confirm('确认退出吗?', '提示', {}).then(() => {
@@ -87,9 +121,17 @@
         })
       },
       // 折叠导航栏
-      collapse  () {
+      collapse() {
         this.isCollapse = !this.isCollapse
+        this.handleChange()
       }
     }
   }
 </script>
+<style>
+  ::-webkit-scrollbar {
+    width: 0; /* 滚动条宽度为0 */
+    height: 0; /* 滚动条高度为0 */
+    display: none; /* 滚动条隐藏 */
+  }
+</style>
