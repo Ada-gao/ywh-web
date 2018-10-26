0071ae33863912e23c36984d449170dfc62fe1b9
android <android>
2018-10-26 15:22:46 +0800

    修复左侧导航栏显示有留白bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,30 @@
 <template>
   <el-container style="height: 100%;">
-    <el-menu class="el-menu-nav" index="0" :collapse="isCollapse" :router="true" default-active="1" @open="handleChange"
-             @close="handleChange">
-      <el-menu-item index="0" disabled style="cursor:default"><i class="fa fa-flash"/><span slot="title">雷鸣外呼管理平台</span>
-      </el-menu-item>
+    <el-menu class="el-menu-nav"text-color="#fff" active-text-color="#fff" index="0" :collapse="isCollapse":unique-opened="true" :router="true" default-active="1" @open="handleChange" @close="handleChange">
+      <el-menu-item index="0" disabled style="cursor:default"><i class="fa fa-flash"/><span slot="title">雷鸣外呼管理平台</span></el-menu-item>
       <el-menu-item index="1" route="/dashboard"><i class="fa fa-home"/><span slot="title">首页-工作台</span></el-menu-item>
-      <el-menu-item index="2" v-if="isSuperAdmin === 'false'" route="/account"><i class="iconfont icon-zhanghu"/><span
-        slot="title">账户管理</span></el-menu-item>
-      <el-menu-item index="3" route="/company"><i class="iconfont icon-gongsiguanli"/><span slot="title">公司管理</span>
-      </el-menu-item>
-      <el-menu-item index="4" route="/salesman"><i class="iconfont icon-xiaoshoushu"/><span slot="title">销售管理</span>
-      </el-menu-item>
-      <el-menu-item index="5" route="/list"><i class="iconfont icon-guize"/><span slot="title">名单管理</span>
-      </el-menu-item>
+      <el-menu-item index="2" v-if="isSuperAdmin === 'false'" route="/account"><i class="iconfont icon-zhanghu"/><span slot="title">账户管理</span></el-menu-item>
+      <el-menu-item index="3" route="/company"><i class="iconfont icon-gongsiguanli"/><span slot="title">公司管理</span></el-menu-item>
+      <el-menu-item index="4" route="/salesman"><i class="iconfont icon-xiaoshoushu"/><span slot="title">销售管理</span></el-menu-item>
+      <el-menu-item index="5" route="/list"><i class="iconfont icon-guize"/><span slot="title">名单管理</span></el-menu-item>
       <el-menu-item index="6" route="/task"><i class="fa fa-phone"/><span slot="title">任务管理</span></el-menu-item>
       <el-submenu index="7">
-        <template slot="title"><i class="iconfont icon-xiaoxishezhi" style="color: #ffffff"></i><span slot="title"
-                                                                                                      style="color: #ffffff">推送管理</span>
-        </template>
-        <el-menu-item index="7-1" route="/push/rule"><i class="iconfont icon-guize"/><span slot="title">推送规则</span>
-        </el-menu-item>
-        <el-menu-item index="7-2" route="/push/push"><i class="iconfont icon-fasong"/><span slot="title">信息推送</span>
-        </el-menu-item>
+        <template slot="title"><i class="iconfont icon-xiaoxishezhi"></i><span slot="title" >推送管理</span></template>
+        <el-menu-item index="7-1" route="/push/rule"><i class="iconfont icon-guize"/><span slot="title">推送规则</span></el-menu-item>
+        <el-menu-item index="7-2" route="/push/push"><i class="iconfont icon-fasong"/><span slot="title">信息推送</span></el-menu-item>
       </el-submenu>
-      <el-menu-item index="8" v-if="isSuperAdmin === 'true'" route="/trial"><i class="fa fa-list-ul"/><span
-        slot="title">试用管理</span></el-menu-item>
-      <el-menu-item index="9" v-if="isSuperAdmin === 'true'" route="/check"><i class="iconfont icon-shenheguanli"/><span
-        slot="title">审核管理</span></el-menu-item>
+      <el-menu-item index="8" v-if="isSuperAdmin === 'true'" route="/trial"><i class="fa fa-list-ul"/><span slot="title">试用管理</span></el-menu-item>
+      <el-menu-item index="9" v-if="isSuperAdmin === 'true'" route="/check"><i class="iconfont icon-shenheguanli"/><span slot="title">审核管理</span></el-menu-item>
       <el-submenu index="10">
-        <template slot="title"><i class="iconfont icon-chart" style="color: #ffffff"></i><span slot="title"
-                                                                                               style="color: #ffffff">报表统计</span>
-        </template>
-        <el-menu-item index="10-1" route="/report/workload"><i class="iconfont icon-wodegongzuoliang"/><span
-          slot="title">工作量统计</span></el-menu-item>
-        <el-menu-item index="10-2" route="/report/task"><i class="iconfont icon-boda"/><span slot="title">外呼任务统计</span>
-        </el-menu-item>
-        <el-menu-item index="10-3" route="/report/count"><i class="iconfont icon-lishi"/><span slot="title">话务统计</span>
-        </el-menu-item>
-        <el-menu-item index="10-4" route="/report/record"><i class="iconfont icon-tonghuahuawuliang"/><span
-          slot="title">历史通话记录</span></el-menu-item>
+        <template slot="title"><i class="iconfont icon-chart"></i><span slot="title">报表统计</span></template>
+        <el-menu-item index="10-1" route="/report/workload"><i class="iconfont icon-wodegongzuoliang"/><span slot="title">工作量统计</span></el-menu-item>
+        <el-menu-item index="10-2" route="/report/task"><i class="iconfont icon-boda"/><span slot="title">外呼任务统计</span></el-menu-item>
+        <el-menu-item index="10-3" route="/report/count"><i class="iconfont icon-lishi"/><span slot="title">话务统计</span></el-menu-item>
+        <el-menu-item index="10-4" route="/report/record"><i class="iconfont icon-tonghuahuawuliang"/><span slot="title">历史通话记录</span></el-menu-item>
       </el-submenu>
-      <el-menu-item index="11" v-if="isSuperAdmin === 'true'" route="/system"><i class="iconfont icon-quanxian"/><span
-        slot="title">系统管理</span></el-menu-item>
-      <el-menu-item index="12" v-if="isSuperAdmin === 'true'" route="/feedback"><i class="fa fa-question-circle"/><span
-        slot="title">问题反馈</span></el-menu-item>
-      <el-menu-item index="13" v-if="isSuperAdmin === 'true'" route="/version/list"><i
-        class="iconfont icon-banben"/><span slot="title">版本管理</span></el-menu-item>
+      <el-menu-item index="11" v-if="isSuperAdmin === 'true'" route="/system"><i class="iconfont icon-quanxian"/><span slot="title">系统管理</span></el-menu-item>
+      <el-menu-item index="12" v-if="isSuperAdmin === 'true'" route="/feedback"><i class="fa fa-question-circle"/><span slot="title">问题反馈</span></el-menu-item>
+      <el-menu-item index="13" v-if="isSuperAdmin === 'true'" route="/version/list"><i class="iconfont icon-banben"/><span slot="title">版本管理</span></el-menu-item>
     </el-menu>
     <el-container :style="{width:'90%',height:pageHeight+'px'}">
       <el-header>
@@ -128,10 +107,34 @@
     }
   }
 </script>
-<style>
+<style lang="scss">
   ::-webkit-scrollbar {
     width: 0; /* 滚动条宽度为0 */
     height: 0; /* 滚动条高度为0 */
     display: none; /* 滚动条隐藏 */
+  }
+  .el-menu{
+    background: #343744;
+  }
+  .el-menu-item.is-disabled{
+    opacity: 1;
+    height: 60px;
+    line-height: 60px;
+  }
+  .el-menu-item.is-active{
+    background: #00C1DF;
+  }
+  .el-menu-item,.el-submenu{
+    background: #42485B;
+  }
+  .el-menu-item:focus,.el-menu-item:hover,.el-submenu__title:focus,.el-submenu__title:hover{
+    background: #00C1DF ;
+  }
+  .el-menu-item i,.el-submenu i{
+    margin-right: 10px;
+  }
+  .el-menu-item i,.el-menu-item span,.el-submenu i,.el-submenu span{
+    color: #fff;
+    font-size: 16px;
   }
 </style>
